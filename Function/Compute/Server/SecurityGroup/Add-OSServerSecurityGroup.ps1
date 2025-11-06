b6c4a21643502b8c42a335fe0211f5e56d0a2cd6
Max Kozlov <{ID}+{username}@users.noreply.github.com>
2025-11-06 16:31:24 +0300

    Add functions for add/remove Server Security Groups

---
+++ Function/Compute/Server/SecurityGroup/Add-OSServerSecurityGroup.ps1
@@ -0,0 +1,64 @@
+﻿<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .PARAMETER Server
+
+    .PARAMETER APIKey
+
+    .INPUTS
+
+    .OUTPUTS
+
+    .EXAMPLE
+
+    .LINK
+
+        https://docs.openstack.org/api-ref/compute/?expanded=delete-metadata-item-detail#add-security-group-to-a-server-addsecuritygroup-action
+
+    .NOTES
+#>
+function Add-OSServerSecurityGroup
+{
+    [CmdLetBinding(DefaultParameterSetName = 'Default')]
+    Param
+    (
+        [Parameter (ParameterSetName = 'Default', Mandatory = $true, ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Server,
+
+        [Parameter (ParameterSetName = 'Default', Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        $SecurityGroup
+    )
+    process
+    {
+        try
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message "start, ParameterSetName [$($PsCmdlet.ParameterSetName)]"
+
+            foreach($Server in $Server)
+            {
+                $Server = Get-OSObjectIdentifierer -Object $Server -PropertyHint 'OS.Server'
+                foreach($SecurityGroup in $SecurityGroup)
+                {
+                    $SecurityGroup = Get-OSObjectIdentifierer -Object $SecurityGroup -PropertyHint 'OS.SecurityGroup'
+
+                    Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type INFO -Message "add SecurityGroup [$SecurityGroup] to Server [$SrvID]"
+
+                    Invoke-OSApiRequest -HTTPVerb Post -Type compute -Uri "servers/$Server/action" -Body @{ addSecurityGroup = @{ name = $SecurityGroup } }
+                }
+            }
+        }
+        catch
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type ERROR -Exception $_
+            throw
+        }
+        finally
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message 'end'
+        }
+    }
+}
---
+++ Function/Compute/Server/SecurityGroup/Remove-OSServerOSServerSecurityGroup.ps1
@@ -0,0 +1,65 @@
+﻿<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .PARAMETER Server
+
+    .PARAMETER APIKey
+
+    .INPUTS
+
+    .OUTPUTS
+
+    .EXAMPLE
+
+    .LINK
+
+        https://docs.openstack.org/api-ref/compute/?expanded=delete-metadata-item-detail#remove-security-group-from-a-server-removesecuritygroup-action
+
+    .NOTES
+#>
+function Remove-OSServerSecurityGroup
+{
+    [CmdLetBinding(DefaultParameterSetName = 'Default')]
+    Param
+    (
+        [Parameter (ParameterSetName = 'Default', Mandatory = $true, ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Server,
+
+        [Parameter (ParameterSetName = 'Default', Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        $SecurityGroup
+    )
+
+    process
+    {
+        try
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message "start"
+
+            foreach($Server in $Server)
+            {
+                $Server = Get-OSObjectIdentifierer -Object $Server -PropertyHint 'OS.Server'
+                foreach($SecurityGroup in $SecurityGroup)
+                {
+                    $SecurityGroup = Get-OSObjectIdentifierer -Object $SecurityGroup -PropertyHint 'OS.SecurityGroup'
+
+                    Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type INFO -Message "remove SecurityGroup [$SecurityGroup] from Server [$SrvID]"
+
+                    Invoke-OSApiRequest -HTTPVerb Post -Type compute -Uri "servers/$Server/action" -Body @{ removeSecurityGroup = @{ name = $SecurityGroup } }
+                }
+            }
+        }
+        catch
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type ERROR -Exception $_
+            throw
+        }
+        finally
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message 'end'
+        }
+    }
+}
